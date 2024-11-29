f88e5d6948793156dd9684f6ba0792ebc24bd665
thuankhanh27cuti <jichunqing1212@gmail.com>
2024-11-29 19:08:46 +0700

    v5

--- controllers/AdminViewUpdateController.js
+++ controllers/AdminViewUpdateController.js
@@ -32,3 +32,123 @@ exports.handleUpdateMonAn = async (req, res) => {
     }
     res.redirect('/admin/mon-an');
 }
+
+exports.updateUser = async (req, res) => {
+    let {id} = req.query;
+    let sql = "SELECT * FROM user WHERE userId = ?";
+    let data = await query.selectAllWithParams(sql, [id]);
+    let user = data[0];
+    let ngaySinh = new Date(user.NgaySinh);
+    if (ngaySinh) {
+        user.NgaySinh = `${ngaySinh.getFullYear()}-${padStart(ngaySinh.getMonth() + 1)}-${padStart(ngaySinh.getDate())}`;
+    }
+    res.render('admin/update/user', {data: user});
+}
+
+exports.handleUpdateUser = async (req, res) => {
+    let {username, password, ho, ten, email, loaiUser, phone, address, ngaySinh} = req.body;
+
+    let isUpdate = true;
+
+    if (email !== "") {
+        let sqlCount = "SELECT COUNT(*) AS count FROM user WHERE email = ? AND UserName != ?";
+        let dataCount = await query.selectAllWithParams(sqlCount, [email, username]);
+        let count = dataCount[0].count;
+
+        isUpdate = count === 0;
+    }
+
+    if (isUpdate) {
+        if (ngaySinh !== "") {
+            let sqlUpdate = "UPDATE user SET PassWord = ?, email = ?, Ho = ?, Ten = ?, DiaChi = ?, SDT = ?, NgaySinh = ?, LoaiUser = ? WHERE UserName = ?";
+            await query.queryWithParams(sqlUpdate, [password, email, ho, ten, address, phone, ngaySinh, loaiUser, username]);
+        }
+        else {
+            let sqlUpdate = "UPDATE user SET PassWord = ?, email = ?, Ho = ?, Ten = ?, DiaChi = ?, SDT = ?, NgaySinh = NULL, LoaiUser = ? WHERE UserName = ?";
+            await query.queryWithParams(sqlUpdate, [password, email, ho, ten, address, phone, loaiUser, username]);
+        }
+    }
+
+    res.redirect('/admin/tai-khoan');
+}
+
+exports.updateNguyenLieu = async (req, res) => {
+    let {id} = req.query;
+
+    let sql = "SELECT * FROM nguyenlieu WHERE MaNL = ?";
+    let select = await query.selectAllWithParams(sql, [id]);
+    let data = select[0];
+
+    res.render('admin/update/nguyenLieu', {data: data});
+}
+
+exports.handleUpdateNguyenLieu = async (req, res) => {
+    let {maNL, tenNL, donGiaNhap, soLuongCon} = req.body;
+
+    let sql = "UPDATE nguyenlieu SET TenNL = ?, DonGiaNhap = ?, soLuongCon = ? WHERE MaNL = ?";
+    await query.queryWithParams(sql, [tenNL, donGiaNhap, soLuongCon, maNL]);
+
+    res.redirect('/admin/nguyen-lieu');
+}
+exports.updateSoLuongNguyenLieu = async (req, res) => {
+    let {id} = req.query;
+
+    let sql = "SELECT * FROM nguyenlieu WHERE MaNL = ?";
+    let select = await query.selectAllWithParams(sql, [id]);
+    let data = select[0];
+
+    let sqlNhaCungCap = "SELECT MaNCC, TenNCC FROM nhacungcap";
+    let dataNhaCungCap = await query.selectAll(sqlNhaCungCap);
+
+    res.render('admin/update/soLuongNguyenLieu', {data: data, nhaCungCap: dataNhaCungCap});
+}
+
+exports.handleUpdateSoLuongNguyenLieu = async (req, res) => {
+    let { maNL, NCC, nhapThem } = req.body;
+
+    let userId = req.session.session.userId;
+    let date = new Date();
+
+    let sqlInsertHoaDon = "INSERT INTO hoadonnhap(MaNCC, userId, NgayNhapHang) VALUES (?, ?, ?)";
+    let maHoaDonNhap = await query.insertAndGetId(sqlInsertHoaDon, [NCC, userId, date]);
+
+    let sql = "SELECT TenNL, DonGiaNhap, SoLuongCon FROM nguyenlieu WHERE MaNL = ?";
+    let select = await query.selectAllWithParams(sql, [maNL]);
+    let data = select[0];
+
+    let donGiaNhap = data.DonGiaNhap;
+    let soLuongCon = data.SoLuongCon;
+
+    let total = parseInt(nhapThem) * donGiaNhap;
+
+    let sqlInsertChiTietHoaDonNhap = "INSERT INTO chitiethoadonnhap(MaHoaDonNhap, MaNL, SoLuongNhap, TongTien) VALUES (?, ?, ?, ?)";
+    await query.queryWithParams(sqlInsertChiTietHoaDonNhap, [maHoaDonNhap, maNL, nhapThem, total]);
+
+    let sqlUpdateSoLuong = "UPDATE nguyenlieu SET SoLuongCon = ? WHERE MaNL = ?";
+    let newSoLuongCon = soLuongCon + parseInt(nhapThem);
+
+    await query.queryWithParams(sqlUpdateSoLuong, [newSoLuongCon, maNL]);
+
+    res.redirect('/admin/nguyen-lieu');
+}
+
+exports.updateGiamGia = async (req, res) => {
+    let {id} = req.query;
+
+    let sql = "SELECT * FROM giamgia WHERE MaGiamGia = ?";
+    let select = await query.selectAllWithParams(sql, [id]);
+    let data = select[0];
+
+    res.render('admin/update/giamGia', {data: data});
+}
+
+exports.handleUpdateGiamGia = async (req, res) => {
+    console.log(req.body);
+    let { maGiamGia, giamGia} = req.body;
+
+    res.redirect('/admin/giam-gia');
+}
+
+const padStart = (number) => {
+    return number.toString().padStart(2, "0");
+};
--- routes/AdminViewUpdateRoutes.js
+++ routes/AdminViewUpdateRoutes.js
@@ -12,4 +12,36 @@ router.post('/mon-an',
     [rolesMiddleware.isAdminAndNhanVienMiddleware, upload.single('file')],
     adminViewUpdateController.handleUpdateMonAn);
 
+router.get('/user',
+    [rolesMiddleware.isAdminAndNhanVienMiddleware],
+    adminViewUpdateController.updateUser);
+
+router.post('/user',
+    [rolesMiddleware.isAdminAndNhanVienMiddleware, upload.single('file')],
+    adminViewUpdateController.handleUpdateUser);
+
+router.get('/nguyen-lieu',
+    [rolesMiddleware.isAdminAndNhanVienMiddleware],
+    adminViewUpdateController.updateNguyenLieu);
+
+router.post('/nguyen-lieu',
+    [rolesMiddleware.isAdminAndNhanVienMiddleware, upload.single('file')],
+    adminViewUpdateController.handleUpdateNguyenLieu);
+
+router.get('/so-luong-nguyen-lieu',
+    [rolesMiddleware.isAdminAndNhanVienMiddleware],
+    adminViewUpdateController.updateSoLuongNguyenLieu);
+
+router.post('/so-luong-nguyen-lieu',
+    [rolesMiddleware.isAdminAndNhanVienMiddleware, upload.single('file')],
+    adminViewUpdateController.handleUpdateSoLuongNguyenLieu);
+
+router.get('/giam-gia',
+    [rolesMiddleware.isAdminAndNhanVienMiddleware],
+    adminViewUpdateController.updateGiamGia);
+
+router.post('/giam-gia',
+    [rolesMiddleware.isAdminAndNhanVienMiddleware, upload.single('file')],
+    adminViewUpdateController.handleUpdateGiamGia);
+
 module.exports = router;
